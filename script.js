6b3bd499a5ba845f870362a965f324101cc666e5
castynet <riungemaina@gmail.com>
2021-06-09 08:17:59 +0300

    script: configured show/hide sidebar

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 import * as fn from "./functions.js";
 
 const openSelector = document.getElementById("typeSelector");
+const closeSelector = document.getElementById("sidebarClose");
+const sidebar = document.getElementById("sidebar");
+const closeBtn = document.getElementById("closeBtn");
+
+var hideElementToRight = "-100%";
 
 openSelector.onclick = () => {
-  fn.openSelector("hello");
+  fn.openSelector(sidebar, closeBtn, "0", "20px");
+};
+
+closeSelector.onclick = () => {
+  fn.openSelector(sidebar, closeBtn, hideElementToRight, hideElementToRight);
 };
