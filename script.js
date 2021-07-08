92d1592dcab1020563144f83e51540a4395abbe1
castynet <riungemaina@gmail.com>
2021-07-09 00:00:02 +0300

    script: added logi to handle open & close for the sidebar underlay

--- script.js
+++ script.js
@@ -4,13 +4,24 @@ const openSelector = document.getElementById("typeSelector");
 const closeSelector = document.getElementById("sidebarClose");
 const sidebar = document.getElementById("sidebar");
 const closeBtn = document.getElementById("closeBtn");
+const sidebarUnderlay = document.getElementById("sidebarUnderlay");
 
 var hideElementToRight = "-100%";
 
-openSelector.onclick = () => {
-  fn.openSelector(sidebar, closeBtn, "0", "20px");
+const handleClose = () => {
+  fn.openSelector(sidebar, closeBtn, hideElementToRight, hideElementToRight);
+  sidebarUnderlay.style.display = "none";
+};
+
+sidebarUnderlay.onclick = () => {
+  handleClose();
 };
 
 closeSelector.onclick = () => {
-  fn.openSelector(sidebar, closeBtn, hideElementToRight, hideElementToRight);
+  handleClose();
+};
+
+openSelector.onclick = () => {
+  fn.openSelector(sidebar, closeBtn, "0", "20px");
+  sidebarUnderlay.style.display = "block";
 };
